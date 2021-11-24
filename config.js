ae650c9047d53f8a71500a1d00e67c78cd54bb6a
Damia <Damia@DESKTOP-9FEBSUJ.local>
2021-11-23 19:06:22 -0700

    Update .gitignore and config_example.js

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    // Bot token
+    token: 'NjQ2MzcyOTEyOTUzMjk0ODgx.XdQLrw.F4eWzV-DUexey1ToYKRiMebcRX8',
+    // mongodb connection string
+    mongoURI: 'mongodb+srv://TheArchive:bAKk5d368jt61OUF@cluster0.7hzdh.mongodb.net/Archive',
+    // activitys not to be tracked
+    notgames: [
+        'Custom Status',
+        'Spotify',
+        '-help',
+        'Shard 8/8',
+        'with myself',
+        'Google Chrome',
+        '@Groovy',
+        'EVGA Precision X1',
+        'OUTOFWORK RP',
+        'Old School RuneScape',
+        '/play'
+    ]
+};
+// example:
+/*
+    module.exports = {
+        // Bot token
+        token: 'somestringprovidedbydiscorddeveloperportal',
+        // mongodb connection string
+        mongoURI: 'mongodb+srv://<Username>:<Password>@mongodb.com/<Database>', 
+        // Things to exclude from being tracked
+        notgames: [
+            'Custom Status',
+            'Spotify',
+        ]
+*/
---
+++ config_example.js
@@ -0,0 +1,12 @@
+// rename me to config.js and edit me
+module.exports = {
+        // Bot token
+        token: 'somestringprovidedbydiscorddeveloperportal',
+        // mongodb connection string
+        mongoURI: 'mongodb+srv://<Username>:<Password>@mongodb.com/<Database>', 
+        // Things to exclude from being tracked
+        notgames: [
+            'Custom Status',
+            'Spotify',
+        ]
+    }
